10ee5f10b2ef5c5c500ad79ffdcde91e2518291c
Rohan Gautam <rohan1gautam@gmail.com>
2017-02-16 00:49:33 +0530

    Update word_combinations.py

--- word_combinations.py
+++ word_combinations.py
@@ -1,4 +1,4 @@
-#this is a program to beat the traditional 'Jumble' newspaper game.
+#This is a program to beat the traditional 'Jumble' newspaper game.
 #the user enters the scrambled word, program outputs 
 #a word that makes sense(is a part of the english dictionary)
 from nltk.corpus import wordnet
@@ -7,13 +7,9 @@ while True:
     letters=raw_input('Enter the letters:')
     if wordnet.synsets(letters):
         print 'Already a word!!'
-    perms=[''.join(i) for i in itertools.permutations(letters)]
-    perms=list(set(perms))
-    x=[i for i in perms if wordnet.synsets(i)]
+    perms=[''.join(i) for i in itertools.permutations(letters) if wordnet.synsets(''.join(i))]
     print 'Possible words are:'
-    for y in x: print y
-
-    
+    for y in perms: print y   
     y=raw_input('Do you want to continue?(y/n)')
     if y=='n':
         print 'Thanks for playing!'

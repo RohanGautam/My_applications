ebe49ee2cd88e03fd8b76d15d140d3713263ef62
Rohan Gautam <rohan1gautam@gmail.com>
2017-02-16 00:44:31 +0530

    Update word_combinations.py

--- word_combinations.py
+++ word_combinations.py
@@ -1,51 +1,24 @@
 #this is a program to beat the traditional 'Jumble' newspaper game.
-#the user enters the scrambled word, program outputs a word that makes sense(is a part of the english dictionary)
-import random
+#the user enters the scrambled word, program outputs 
+#a word that makes sense(is a part of the english dictionary)
 from nltk.corpus import wordnet
+import itertools
 while True:
     letters=raw_input('Enter the letters:')
     if wordnet.synsets(letters):
         print 'Already a word!!'
-        break
-    unique_letters=list(set(letters))    
-    def factorial(x):
-        ans=1
-        while x>0:
-            ans*=x
-            x-=1
-        return ans
-    length=len(letters)
-    n=factorial(length)
-    L=[]
+    perms=[''.join(i) for i in itertools.permutations(letters)]
+    perms=list(set(perms))
+    x=[i for i in perms if wordnet.synsets(i)]
+    print 'Possible words are:'
+    for y in x: print y
+
     
-    while n<>len(L):
-        x=random.sample(range(length),length)
-        if x not in L:
-            L.append(x)
-    
-    
-    outcomes=[]
-    for x in L:
-        st=''
-        for i in x:
-            st+=letters[i]
-        if st not in outcomes:
-            outcomes.append(st)
-    outcomes.sort()
-    
-    d={}
-    for x in unique_letters:
-        d[x]=[y for y in outcomes if y[0]==x]
-    print 'Possible words are:' 
-    for a in d:       
-        for i in d[a]:
-            if wordnet.synsets(i):
-                print i,'\t',    
-    print '\n'
-    x=raw_input('Do you want to continue?(y/n)')
-    if x=='n':
+    y=raw_input('Do you want to continue?(y/n)')
+    if y=='n':
         print 'Thanks for playing!'
         break
 print '<------------------------>'
+
 
 
